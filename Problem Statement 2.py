a3acc0a5860f13dcf785b6c49c917d3141575e9d
Rohit Shinde <62435594+rohit180301@users.noreply.github.com>
2020-05-13 02:54:24 +0530

    Update Problem Statement 2.py

--- Problem Statement 2.py
+++ Problem Statement 2.py
@@ -36,7 +36,6 @@ print(countries_list)
 a1 = ['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80 and above']
 for item in  countries_list:
      b=[]
-     b.clear() 
      for i in range(0,len(Age_group)):          
         if Age_group.iloc[i][0]==item:
             b.extend(list(Age_group.iloc[i]))
